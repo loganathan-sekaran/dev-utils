148dfed64c2ed55a883288f20a61ea556a0c2a66
Loganathan Sekaran <loganathan001@gmail.com>
2024-03-22 12:52:12 +0530

    Added java migration util

---
+++ migration_config.py
@@ -0,0 +1,13 @@
+git_base_dir="c:/1/git"
+modules_paths_list_in_order = [
+
+'commons/kernel/kernel-bom',
+'commons/kernel/kernel-core',
+'commons/kernel/kernel-logger-logback',
+'mosip-openid-bridge/kernel/kernel-openid-bridge-api',
+'mosip-openid-bridge/kernel/kernel-auth-adapter',
+'audit-manager/kernel/kernel-auditmanager-api',
+'commons/kernel/kernel-dataaccess-hibernate',
+'audit-manager/kernel/kernel-auditmanager-service'
+
+]
