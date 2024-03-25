5aa8eff30a76c7ecffa3d6bb1992ece70cc56f4c
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2024-03-25 16:09:12 +0530

    Added migration options
    Signed-off-by: Loganathan Sekar <Loganathan.Sekar@mindtree.com>

--- java-migration-util.py
+++ java-migration-util.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 import sys
-from migration_config import modules_paths_list_in_order
-from migration_config import git_base_dir
+from migration_config import modules_paths_list_in_order, git_base_dir, git_repos_in_order, branch
+from text_changes import pom_changes_dict, java_file_changes_dict
 import argparse
 
 parser=argparse.ArgumentParser()
@@ -10,11 +10,22 @@ parser=argparse.ArgumentParser()
 parser.add_argument("--mvnCleanInstall", "-mci", help="Maven Clean Install", nargs='?', const="True")
 parser.add_argument("--mvnCleanInstallSkipTests", "-mcist", help="Maven Clean Install with Skip tests and javadoc in build", nargs='?', const="True")
 parser.add_argument("--gitBaseDir", "-gbd", help="Git Base Directory", default=None)
-parser.add_argument("--commitAndPushToOrigin", "-cpo", help="Commit and push to origin", nargs='?', const="True")
-parser.add_argument("--commitMessage", "-msg", help="Commit and push to origin", default='Committing changes.')
-parser.add_argument("--listModules", "-ls", help="List Modules", nargs='?', const="True")
-parser.add_argument("--buildFromIndex", "-i", help="The index of module from which build to start", default=1, nargs='?', const="True")
+parser.add_argument("--addCommitAndPushToOrigin", "-cpo", help="Add changes, commit and push to origin", nargs='?', const="True")
+parser.add_argument("--add", "-ad", help="Add changes", nargs='?', const="True")
+parser.add_argument("--commit", "-cm", help="Commit", nargs='?', const="True")
+parser.add_argument("--pushToOrigin", "-po", help="push to origin", nargs='?', const="True")
+parser.add_argument("--commitMessage", "-m", help="Commit and push to origin", default='Committing changes.')
+parser.add_argument("--listModules", "-lsm", help="List Modules", nargs='?', const="True")
+parser.add_argument("--listGitRepos", "-lsr", help="List Git Repos", nargs='?', const="True")
+parser.add_argument("--moduleIndex", "-im", help="The index of module from which build to start", default=1, nargs='?', const="True")
+parser.add_argument("--repoIndex", "-ir", help="The index of repo from which related git action to start", default=1, nargs='?', const="True")
 parser.add_argument("--migrate", "-mig", help="Migrate", nargs='?', const="True")
+
+parser.add_argument("--status", "-st", help="git status on all modules", nargs='?', const="True")
+
+parser.add_argument("--diff", "-df", help="git diff on all modules", nargs='?', const="True")
+
+
 
 args=parser.parse_args()
 
@@ -22,7 +33,9 @@ mvnBuildCommand = []
 
 gitAddAllCommand = ['git', 'add', '-u']
 gitCommitCommand = ['git', 'commit','--allow-empty', '-m', args.commitMessage]
-gitPushCommand = ['git', 'push', 'origin']
+gitPushCommand = ['git', 'push', 'origin', branch]
+gitStatusCommand = ['git', 'status']
+gitDiffCommand = ['git', '--no-pager', 'diff']
 
 if args.mvnCleanInstall is not None:
     print("Maven Clean Install")
@@ -41,8 +54,11 @@ else:
 
 print("Git Base Directory: " + gitBaseDir)
 
-module_start_index=int(args.buildFromIndex) - 1
+module_start_index=int(args.moduleIndex) - 1
 modules_to_build=modules_paths_list_in_order[module_start_index::]
+
+repo_start_index=int(args.repoIndex) - 1
+git_repos=git_repos_in_order[repo_start_index::]
 
 
 def buildAll():
@@ -53,26 +69,60 @@ def listModules():
     print("List of modules:")
     for i, module in enumerate(modules_to_build):
         print("[" + str(i + 1) + "/" + str(len(modules_to_build))  + "]: " + module)
+        
+def listGitRepos():
+    print("List of Git Repositoris:")
+    for i, module in enumerate(git_repos):
+        print("[" + str(i + 1) + "/" + str(len(git_repos))  + "]: " + module)
 
 def buildModule(index, modulePath):
     print(">>>>>>> Building module [" + str(index + 1) + "/" + str(len(modules_to_build))  + "]: " + modulePath)
-    moduleFullPath=getModuleFullPath(modulePath)
+    moduleFullPath=getFullPath(modulePath)
     runCommand(mvnBuildCommand, moduleFullPath)
    
-def pushAll():
-    for i, module in enumerate(modules_to_build):
-        pushModule(i, module)
+def addCommitAndPushAll():
+    for i, repo in enumerate(git_repos):
+        addCommitAndPushRepo(i, repo)
 
-def pushModule(index, modulePath):
-    print(">>>>>>> Pushing module [" + str(index + 1) + "/" + str(len(modules_to_build))  + "]: " + modulePath)
-    moduleFullPath=getModuleFullPath(modulePath)
-    print("Committing")
-    runCommand(gitAddAllCommand, moduleFullPath)
-    runCommand(gitCommitCommand, moduleFullPath)
-    runCommand(gitPushCommand, moduleFullPath)
+def addCommitAndPushRepo(index, repo):
+    print(">>>>>>> Working on repo [" + str(index + 1) + "/" + str(len(git_repos))  + "]: " + repo)
+    repoFullPath=getFullPath(repo)
     
-def getModuleFullPath(modulePath):
-    return gitBaseDir + '/' + modulePath
+    if args.add is not None:
+        print("Adding all tracked changes")
+        runCommand(gitAddAllCommand, repoFullPath)
+    
+    if args.commit is not None:
+        print("Committing changes")
+        runCommand(gitCommitCommand, repoFullPath)
+        
+    if args.pushToOrigin is not None:
+        print("Pushing changes")
+        runCommand(gitPushCommand, repoFullPath)
+    
+    
+def gitStatusRepos():
+    for i, repo in enumerate(git_repos):
+        gitStatusRepo(i, repo)
+
+def gitStatusRepo(index, repo):
+    print(">>>>>>> Status of Repo [" + str(index + 1) + "/" + str(len(git_repos))  + "]: " + repo)
+    repoFullPath=getFullPath(repo)
+    runCommand(gitStatusCommand, repoFullPath)
+    
+    
+def gitDiffRepos():
+    for i, repo in enumerate(git_repos):
+        gitDiffRepo(i, repo)
+
+def gitDiffRepo(index, repo):
+    print(">>>>>>> Diff of Repo [" + str(index + 1) + "/" + str(len(git_repos))  + "]: " + repo)
+    repoFullPath=getFullPath(repo)
+    runCommand(gitDiffCommand, repoFullPath)
+    
+    
+def getFullPath(relativePath):
+    return gitBaseDir + '/' + relativePath
     
 def runCommand(command, moduleFullPath):
     subprocess.run(command, cwd=moduleFullPath, shell=True, check=True)
@@ -99,15 +149,28 @@ def replace_text_in_folder(folder_path, old_text, new_text, file_extension):
     print(f"Replaced '{old_text}' with '{new_text}' in all '{file_extension}' files within '{folder_path}'.")
 
 def migratePoms():
+    print(">>>>>>> Migrating POMs")
     for i, module in enumerate(modules_to_build):
-        migratePomInModule(i, module)
+        migratePomsInModule(i, module)
         
-def migratePomInModule(index, modulePath):
+def migratePomsInModule(index, modulePath):
     print(">>>>>>> Migrating POM in module [" + str(index + 1) + "/" + str(len(modules_to_build))  + "]: " + modulePath)
-    moduleFullPath=getModuleFullPath(modulePath)
-    replace_text_in_folder(moduleFullPath, "<maven.compiler.source>11</maven.compiler.source>", "<maven.compiler.source>21</maven.compiler.source>", "pom.xml")
-    replace_text_in_folder(moduleFullPath, "<maven.compiler.target>11</maven.compiler.target>", "<maven.compiler.target>21</maven.compiler.target>", "pom.xml")
-    replace_text_in_folder(moduleFullPath, "1.2.1-SNAPSHOT", "1.2.1-java21-SNAPSHOT", "pom.xml")
+    moduleFullPath=getFullPath(modulePath)
+    for key,value in pom_changes_dict.items():
+        replace_text_in_folder(moduleFullPath, key, value, "pom.xml")
+
+def migrateJavaFiles():
+    print(">>>>>>> Migrating Java Files")
+    for i, module in enumerate(modules_to_build):
+        migrateJavaFilesInModule(i, module)
+        
+def migrateJavaFilesInModule(index, modulePath):
+    print(">>>>>>> Migrating Java files in module [" + str(index + 1) + "/" + str(len(modules_to_build))  + "]: " + modulePath)
+    moduleFullPath=getFullPath(modulePath)
+    
+    for key,value in java_file_changes_dict.items():
+        replace_text_in_folder(moduleFullPath, key, value, ".java")
+
 
 def migrate():
     print(f"Migrating to Java 21")
@@ -123,13 +186,29 @@ def main():
     if len(mvnBuildCommand) > 0:
         buildAll()
         print(">>>>>>> Build completed.")
+        
+    if args.add is not None or args.commit is not None or args.pushToOrigin is not None:
+        addCommitAndPushAll()
+        print(">>>>>>> Commit completed.")
     
-    if args.commitAndPushToOrigin is not None:
-        pushAll()
-        print(">>>>>>> Push to Origin completed.")
+    if args.addCommitAndPushToOrigin is not None:
+        args.add = True
+        args.commit = True
+        args.pushToOrigin = True
+        addCommitAndPushAll()
+        print(">>>>>>> Commit and Push to Origin completed.")
         
     if args.listModules is not None:
         listModules()
+        
+    if args.listGitRepos is not None:
+        listGitRepos()
+        
+    if args.status is not None:
+        gitStatusRepos()
+        
+    if args.diff is not None:
+        gitDiffRepos()
 
 
 main()
--- migration_config.py
+++ migration_config.py
@@ -1,4 +1,7 @@
 git_base_dir="c:/1/git"
+
+branch = "MOSIP-31617-java-21-migration"
+
 modules_paths_list_in_order = [
 
 'commons/kernel/kernel-bom',
@@ -9,5 +12,13 @@ modules_paths_list_in_order = [
 'audit-manager/kernel/kernel-auditmanager-api',
 'commons/kernel/kernel-dataaccess-hibernate',
 'audit-manager/kernel/kernel-auditmanager-service'
+
+]
+
+git_repos_in_order = [
+
+'commons',
+'mosip-openid-bridge',
+'audit-manager',
 
 ]
